7ee163bd4bb1ced3c3d8341efc5e9ec3a943e5a6
heiliuer <heiliuer@github.com>
2016-08-01 17:48:36 +0800

    fixed

--- plugin/content_script.js
+++ plugin/content_script.js
@@ -116,18 +116,37 @@ $(function () {
 
     //设置企业号密码
     if (isDomain("qq.com")) {
-        setTimeout(function () {
-            try {
-                var $docFrame = $(window.frames[0].document);
-                $docFrame.ready(function () {
-                    $docFrame.find("[type='password']").val("wanghao0923").trigger("change input");
-                    $docFrame.find(".mod-set-pwd__submit").removeClass("button-primary_disabled")
-                });
-            } catch (e) {
-                console.error(e);
+        var delay = 1200;
+        var timer;
+        var password = "wanghao0923";
+        function autoInputPwd() {
+            if(timer){
+                clearTimeout(timer);
             }
-        }, 2100);
-    }
+            timer=setTimeout(function () {
+                try {
+                    var $docFrame = $(window.frames[0].document);
+                    $docFrame.ready(function () {
+                        var $input = $docFrame.find("[type='password']");
+                        $input.val(password).trigger("change input");
+                        $docFrame.find(".mod-set-pwd__submit,.js_confirm").removeClass("button-primary_disabled disabled").removeAttr("disabled");
+                        if(!$input.length){
+                            autoInputPwd();
+                        }
+                    });
+                } catch (e) {
+                    autoInputPwd();
+                }
 
+                var $input = $("[type='password']").val(password).trigger("change input");
+                $(".mod-set-pwd__submit,.js_confirm").removeClass("button-primary_disabled disabled").removeAttr("disabled");
+                if(!$input.length){
+                    autoInputPwd();
+                }
+
+            }, delay);
+        }
+        autoInputPwd();
+    }
 
 })
