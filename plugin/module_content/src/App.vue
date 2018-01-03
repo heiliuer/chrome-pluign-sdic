2f50626918a4eedd9c5611865dcdc9beee46f421
wanghao51 <heiliuer@qq.com>
2018-01-03 14:13:24 +0800

    fixed

---
+++ plugin/module_content/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="com_heiliuer_sdic_app">
+        <h2>com_heiliuer_sdic_app</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+    h2 {
+        color: #f00;
+    }
+</style>
---
+++ plugin/module_content/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#plugin-heiliuer-sdic',
+  render: h => h(App)
+})
